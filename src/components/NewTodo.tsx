b0eb8ef29c9698c53ddd64b2d63f1b471fe4ada3
danielleconeva <dconewa@gmail.com>
2025-09-18 10:39:57 +0300

    implement remove functionality and integrate Context API for state management

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,16 +1,30 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 
-const NewTodo = () => {
-    const todoTextInputRef = useRef();
+import { TodosContext } from "../store/todos-context";
+import classes from "./NewTodo.module.css";
+
+const NewTodo: React.FC = () => {
+    const todosCtx = useContext(TodosContext);
+
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
+
+        const enteredText = todoTextInputRef.current!.value;
+
+        if (enteredText.trim().length === 0) {
+            // throw an error
+            return;
+        }
+
+        todosCtx.addTodo(enteredText);
     };
 
     return (
-        <form onSubmit={submitHandler}>
-            <label htmlFor="text">Todo Text</label>
-            <input type="text" id="text" />
+        <form onSubmit={submitHandler} className={classes.form}>
+            <label htmlFor="text">Todo text</label>
+            <input type="text" id="text" ref={todoTextInputRef} />
             <button>Add Todo</button>
         </form>
     );
